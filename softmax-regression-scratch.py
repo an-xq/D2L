fd0b0d7b381ea7d3bb1d15e4b7c438c99ef1fc5a
An <836086513@qq.com>
2024-05-10 15:08:40 +0800

    softmax-regression-scratch完整20240510

--- softmax-regression-scratch.py
+++ softmax-regression-scratch.py
@@ -127,10 +127,81 @@ def evaluate_accuracy(net,data_iter):#计算在指定数据集上模型的精度
             metric.add(accuracy(net(X), y),y.numel())#例如正确数是3，y.numel()元素个数是10，args就是(3,10)，打包了
     return metric[0]/metric[1]#返回精度，即正确率
 
+#7.训练
 
+class Animator:  #绘图类
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
 
-y = torch.tensor([0, 2])
-y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        d2l.plt.pause(0.1)
+        display.display(self.fig)
+        display.clear_output(wait=True)
 
-print(accuracy(y_hat, y) / len(y))
-print(evaluate_accuracy(net, test_iter))
+def train_epoch_ch3(net,train_iter,loss,updater):
+    if isinstance(net,torch.nn.Module):
+        net.train()
+    metric=Accumulator(3)
+    for X,y in train_iter:
+        y_hat=net(X)
+        l=loss(y_hat,y)
+        if isinstance(updater,torch.optim.Optimizer):
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()),accuracy(y_hat,y),y.numel())#加入本batch的总交叉熵，预测正确的个数，batch总数
+    return metric[0]/metric[2],metric[1]/metric[2]#返回平均交叉熵和预测成功的比例
+
+def train_ch3(net,train_iter,test_iter,loss,num_epochs,updater):
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics=train_epoch_ch3(net,train_iter,loss,updater)
+        test_acc=evaluate_accuracy(net,test_iter)
+        animator.add(epoch+1,train_metrics+(test_acc,))
+    train_loss,train_acc=train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+lr=0.1
+def updater(batch_size):
+    return d2l.sgd([W,b],lr,batch_size)
+
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+
