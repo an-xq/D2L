b7200f388cce6c4b5ba611b6f6dcdb3b96d07c46
An <836086513@qq.com>
2024-08-14 13:49:38 +0800

    underfit-overfit参数调整20240814

--- test.py
+++ test.py
@@ -1,18 +1,26 @@
 import numpy as np
 import torch
 
-x1=3
-x2=np.array([1,2,3])
-x3=np.zeros((5,2))
-for i in range(5):
-    x3[i][0]=i
-    x3[i][1]=i*i
-print(x3)
-x5=x3.sum(axis=1)
-print(x5)
+# x1=3
+# x2=np.array([1,2,3])
+# x3=np.zeros((5,2))
+# for i in range(5):
+#     x3[i][0]=i
+#     x3[i][1]=i*i
+# print(x3)
+# x5=x3.sum(axis=1)
+# print(x5)
 
 
 # x4=np.arange(10)
 # print(x4)
 
 # print(np.power(x3,x2.reshape(1,-1)))
+
+train_features=torch.normal(0,0.1,(2,2))
+train_features[0][0]=1
+train_features[0][1]=2
+train_features[1][0]=3
+train_features[1][1]=4
+
+print(train_features[:,1]/2)
--- underfit-overfit.py
+++ underfit-overfit.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from torch import nn
+from IPython import display #图像显示
 
 
 #数据生成
@@ -15,16 +16,22 @@ n_test=100
 batch_size=10
 
 true_w=np.zeros(max_degree)
-true_w[0:4]=np.array([5,1.2,-3.4/math.factorial(2),5.6/math.factorial(3)])
+true_w[0:4]=np.array([5,1.2,-3.4,5.6])
+print(" ".join(f"{value:.6f}" for value in true_w))
 #训练数据
 train_features=torch.normal(0,1,(n_train,1))
 temp_train_labels=np.power(train_features,np.arange(max_degree))
+for i in range(max_degree):
+    temp_train_labels[:,i]/=math.gamma(i+1)
 train_labels=temp_train_labels @ torch.from_numpy(true_w)
 train_labels=train_labels.float()
 train_labels+=torch.normal(0,0.01,train_labels.shape)
+
 #验证数据
 test_features=torch.normal(0,1,(n_test,1))
 temp_test_labels=np.power(test_features,np.arange(max_degree))
+for i in range(max_degree):
+    temp_test_labels[:,i]/=math.gamma(i+1)
 test_labels=temp_test_labels @ torch.from_numpy(true_w)
 test_labels=test_labels.float()
 test_labels+=torch.normal(0,0.01,test_labels.shape)
@@ -35,7 +42,7 @@ def data_iter(data_arrs,batch_size,is_train=True):
     return data.DataLoader(dataset,batch_size,shuffle=is_train)
 
 train_iter=data_iter([train_features,train_labels],batch_size)
-test_iter=data_iter([test_features,test_labels],batch_size)
+test_iter=data_iter([test_features,test_labels],batch_size,is_train=False)
 
 #损失
 loss = nn.MSELoss()
@@ -43,17 +50,18 @@ loss = nn.MSELoss()
 #优化算法
 def sgd(param,lr,batch_size):
     with torch.no_grad():
-        # print(param.grad)
         param-=lr*param.grad/batch_size
         param.grad.zero_()
 
 #模型
 def net(X,w,num_w):
     temp=np.power(X,np.arange(num_w))
+    for i in range(num_w):
+        temp[:,i]/=math.gamma(i+1)
     temp=temp.float()
     fact_w=w[0:num_w]
-    # fact_w=fact_w.float()
     return temp @ fact_w
+
 
 #工具
 def evaluate_loss(net,data_iter,w,num_w,loss):
@@ -65,21 +73,34 @@ def evaluate_loss(net,data_iter,w,num_w,loss):
     return metric[0]/metric[1]
 
 #训练
-epochs=50
-num_w=4
-lr=0.1
-w=torch.normal(0,0.01,size=(max_degree,),requires_grad=True)
+epochs=1500
+num_w=20
+lr=0.01
+# w = torch.nn.Linear(num_w,1).weight[0,:]
+# print(w)
+# w=torch.normal(0,0.01,size=(num_w,),requires_grad=True)
+w = torch.tensor([ 0.1443,  0.0954,  0.0023,  0.1622, -0.0402, -0.0688,  0.0541,  0.1384,
+        -0.0383,  0.1708, -0.0298,  0.0262, -0.0790,  0.0518, -0.1726,  0.1879,
+        -0.1540, -0.0822,  0.0842, -0.1048],requires_grad=True)
+animator = d2l.Animator(xlabel='epoch', ylabel='loss', yscale='log',
+                            xlim=[1, epochs], ylim=[1e-3, 1e2],
+                            legend=['train', 'test'])
 for epoch in range(epochs):
     for X,y in train_iter:
         y_hat=net(X,w,num_w)
         l=loss(y_hat,y)
-        l.mean().backward()
+        l.backward()
         sgd(w,lr,batch_size)
+    # print(w)
     train_loss=evaluate_loss(net,train_iter,w,num_w,loss)
     test_loss=evaluate_loss(net,test_iter,w,num_w,loss)
     print("epoch:",epoch)
     print("train_loss:",train_loss)
     print("test_loss:",test_loss)
+    if epoch == 0 or (epoch + 1) % 20 == 0:
+        animator.add(epoch + 1, (train_loss,test_loss))
+
+print(" ".join(f"{value:.6f}" for value in w))
     
 
 
